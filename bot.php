2b9c50ea76afffa7dd019489fe1485a0530e2adc
TheGenocide <oofhack@hotmail.com>
2018-03-28 15:09:33 +0700

    add define url reply

--- bot.php
+++ bot.php
@@ -14,12 +14,11 @@
 			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent			
 				$text = $event['message']['text'];			// Get replyToken			
 				$replyToken = $event['replyToken'];			// Build message to reply back			
-				$messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender		 
-				$url = 'https://api.line.me/v2/bot/message/reply';			
+				$messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender		 	
 				$data = ['replyToken' => $replyToken,'messages' => [$messages],];			
 				$post = json_encode($data);			
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . ACCESS_TOKEN);			
-				$ch = curl_init($url);			
+				$ch = curl_init(URL_REPLY);			
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 	/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
 	define('CHANNEL_ID','1568582442'); //Channel ID
 	define('CHANNEL_SECRET','641262f118c98ac5f497e534bf12119e'); //Channel secret
-	define('ACCESS_TOKEN','cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU='); //Channel access token
+	define('ACCESS_TOKEN','cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU='); //Channel
+	define('URL_REPLY','https://api.line.me/v2/bot/message/reply'); 
 	
 	// ปิดการแจ้ง error
 	//ini_set('display_errors', 1);
