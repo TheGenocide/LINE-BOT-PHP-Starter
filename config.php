bb8275c94350db5bbc53478880a7039d5e0d1f87
TheGenocide <oofhack@hotmail.com>
2018-03-28 15:47:28 +0700

    edit config2

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 	define('URL_MULTICAST','https://api.line.me/v2/bot/message/multicast'); 
 	
 	// ปิดการแจ้ง error
-	//ini_set('display_errors', 1);
-	//ini_set('display_startup_errors', 1);
-	//error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 ?>
