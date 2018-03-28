3cee2327bedb62366c390ca1c449db6400fcf4be
TheGenocide <oofhack@hotmail.com>
2018-03-28 11:41:44 +0700

    add config

--- bot.php
+++ bot.php
@@ -1,33 +1,34 @@
 <?php
-  $access_token = 'cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU=';
-  // Get POST body content
-  $content = file_get_contents('php://input');
-  // Parse JSON
-  $events = json_decode($content, true);
-  // Validate parsed JSON data
-  if (!is_null($events['events'])) {	
-    // Loop through each event	
-    foreach ($events['events'] as $event) {		
-      // Reply only when message sent is in 'text' format		
-      if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent			
-        $text = $event['message']['text'];			// Get replyToken			
-        $replyToken = $event['replyToken'];			// Build message to reply back			
-        $messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender		 
-        $url = 'https://api.line.me/v2/bot/message/reply';			
-        $data = ['replyToken' => $replyToken,'messages' => [$messages],];			
-        $post = json_encode($data);			
-        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
-        $ch = curl_init($url);			
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-        $result = curl_exec($ch);			
-        curl_close($ch);			
-        echo $result . "";		
-      }	
-    }
-  }
+	include_once "config.php";
+	
+	// Get POST body content
+	$content = file_get_contents('php://input');
+	// Parse JSON
+	$events = json_decode($content, true);
+	// Validate parsed JSON data
+	if (!is_null($events['events'])) {	
+		// Loop through each event	
+		foreach ($events['events'] as $event) {		
+			// Reply only when message sent is in 'text' format		
+			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent			
+				$text = $event['message']['text'];			// Get replyToken			
+				$replyToken = $event['replyToken'];			// Build message to reply back			
+				$messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender		 
+				$url = 'https://api.line.me/v2/bot/message/reply';			
+				$data = ['replyToken' => $replyToken,'messages' => [$messages],];			
+				$post = json_encode($data);			
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . ACCESS_TOKEN);			
+				$ch = curl_init($url);			
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
+				$result = curl_exec($ch);			
+				curl_close($ch);			
+				echo $result . "";		
+			}	
+		}
+	}
   echo "OK";
 ?>
---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+	define('CHANNEL_ID','1568582442'); //Channel ID
+	define('CHANNEL_SECRET','641262f118c98ac5f497e534bf12119e'); //Channel secret
+	define('ACCESS_TOKEN','cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU='); //Channel access token
+	
+	// ปิดการแจ้ง error
+	//ini_set('display_errors', 1);
+	//ini_set('display_startup_errors', 1);
+	//error_reporting(E_ALL);
+?>
