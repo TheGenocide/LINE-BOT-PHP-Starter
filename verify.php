8bd44fca82e9d90d5f590f5cdef0ccc212f95419
TheGenocide <oofhack@hotmail.com>
2018-03-28 10:41:43 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
   $access_token = 'cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU=';
-  $url = 'https://api.line.me/v2/oauth/verify';
+  $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
