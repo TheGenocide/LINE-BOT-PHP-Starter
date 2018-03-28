9570fca5d85a89f59e68d87bc718ff33f7234816
TheGenocide <oofhack@hotmail.com>
2018-03-28 21:07:30 +0700

    update wording

--- botApi.php
+++ botApi.php
@@ -1,5 +1,6 @@
 <?php
 	include_once "includes/v3_line_config.php";
+	include_once "core.php";
 	
 	//Default Var
 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . ACCESS_TOKEN);
@@ -55,29 +56,4 @@
 		}
 	}
 	echo "BOT OK";
-	
-	function sendMessage($header,$method,$service,$userId,$data){
-		//$service:1 = reply
-		//$service:2 = push
-		$url = "";
-		$post = json_encode($data);
-		switch($service){
-			case 1:
-				$url = URL_REPLY;
-				break;
-			case 2:
-				$url = URL_PUSE;
-				break;
-		}
-		
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);			
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);			
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-		$result = curl_exec($ch);			
-		curl_close($ch);			
-		echo $result . "";	
-	}
 ?>
