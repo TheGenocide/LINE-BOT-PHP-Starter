271fd59720adaab58189266c06f8f93707dc13a1
TheGenocide <oofhack@hotmail.com>
2018-03-28 15:46:15 +0700

    edit config

--- config.php
+++ config.php
@@ -4,6 +4,8 @@
 	define('CHANNEL_SECRET','641262f118c98ac5f497e534bf12119e'); //Channel secret
 	define('ACCESS_TOKEN','cJKdyIbmbzVAK91DtllUd4vBBOp7RgvaqxraUkBzRV9CaVgZAey+g4awFInI27+RlBTb9VLJsrpF328chNVoJEgzqRqK1ycKd6JYSXGDOcI8y3w2NLYXYzSnC2z2bcJgGiHzms4ZYHglml8/rDrHCwdB04t89/1O/w1cDnyilFU='); //Channel
 	define('URL_REPLY','https://api.line.me/v2/bot/message/reply'); 
+	define('URL_PUSH','https://api.line.me/v2/bot/message/push'); 
+	define('URL_MULTICAST','https://api.line.me/v2/bot/message/multicast'); 
 	
 	// ปิดการแจ้ง error
 	//ini_set('display_errors', 1);
